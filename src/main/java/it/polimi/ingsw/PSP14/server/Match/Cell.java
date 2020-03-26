a10a119bc01f9fad9b1821e7c3a2f9f04ec125f7
luigifusco <luigifusco98@gmail.com>
2020-03-26 18:24:58 +0100

    add JavaDoc for all classes

--- src/main/java/it/polimi/ingsw/PSP14/server/Match/Cell.java
+++ src/main/java/it/polimi/ingsw/PSP14/server/Match/Cell.java
@@ -1,21 +1,36 @@
 package it.polimi.ingsw.PSP14.server.Match;
 
+/**
+ * Contains information about a single cell of the board.
+ */
 public class Cell {
     private int towerSize = 0;
     private boolean isCompleted = false;
 
+    /**
+     * @return the current tower size.
+     */
     public int getTowerSize() {
         return towerSize;
     }
 
+    /**
+     * @return a truth value indicating if the dome is built.
+     */
     public boolean getIsCompleted() {
         return isCompleted;
     }
 
+    /**
+     * Increases by one the size of the tower.
+     */
     public void incrementTowerSize() {
         towerSize++;
     }
 
+    /**
+     * Builds the dome.
+     */
     public void setAsCompleted() {
         isCompleted = true;
     }
