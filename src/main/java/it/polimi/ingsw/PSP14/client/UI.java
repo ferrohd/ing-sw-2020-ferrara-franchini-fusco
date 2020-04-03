16b08b33a95103e49f84705de82cdbae6550af9f
Alessandro <aleferrara1998@gmail.com>
2020-04-03 15:38:28 +0200

    switch from Array to ArrayList

--- src/main/java/it/polimi/ingsw/PSP14/client/UI.java
+++ src/main/java/it/polimi/ingsw/PSP14/client/UI.java
@@ -1,39 +1,32 @@
 package it.polimi.ingsw.PSP14.client;
 
-import it.polimi.ingsw.PSP14.core.Cell;
+import it.polimi.ingsw.PSP14.core.model.Cell;
 import it.polimi.ingsw.PSP14.core.Player;
-import it.polimi.ingsw.PSP14.core.Point;
+import it.polimi.ingsw.PSP14.core.model.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public abstract class   UI {
 
-    Player[] players;
+    ArrayList<Player> players;
 
-    public UI(int numberOf_players) {
-        players = new Player[numberOf_players];
+    public UI(Collection<Player> players) {
+        this.players.addAll(players);
     }
 
     /**
      * @param newPlayer new player to be registered in the view
      */
     public void registerPlayer(Player newPlayer) {
-        for(int i = 0; i < players.length; i++) {
-            if (players[i] == null) {
-                players[i] = newPlayer;
-                break;
-            }
-        }
+        players.add(newPlayer);
     }
 
     /**
      * @param removedPlayer existing player to be removed from the view
      */
     public void removePlayer(Player removedPlayer) {
-        for(int i = 0; i < players.length; i++) {
-            if (players[i].equals(removedPlayer)) {
-                players[i] = null;
-                break;
-            }
-        }
+        players.remove(removedPlayer);
     }
 
     public abstract void drawBoardInit(Cell[][] board);
