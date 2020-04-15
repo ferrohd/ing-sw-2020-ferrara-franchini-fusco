7017150f03fd8ee308dab98b6c103814e30d1f55
luigifusco <luigifusco98@gmail.com>
2020-04-15 12:55:37 +0200

    make BuildAction proposable

--- src/main/java/it/polimi/ingsw/PSP14/server/actions/BuildAction.java
+++ src/main/java/it/polimi/ingsw/PSP14/server/actions/BuildAction.java
@@ -1,13 +1,24 @@
 package it.polimi.ingsw.PSP14.server.actions;
 
+import it.polimi.ingsw.PSP14.core.proposals.BuildProposal;
 import it.polimi.ingsw.PSP14.server.model.Cell;
 import it.polimi.ingsw.PSP14.server.model.Match;
 import it.polimi.ingsw.PSP14.server.model.Point;
 import it.polimi.ingsw.PSP14.server.model.TowerSizeException;
 
-public class BuildAction extends Action {
+public class BuildAction extends Action implements Proposable {
     private Point point;
     private boolean dome;
+
+    public BuildAction(String user, Point point, boolean dome) {
+        super(user);
+        this.point = point;
+        this.dome = dome;
+    }
+
+    public BuildProposal getProposal() {
+        return new BuildProposal(point, dome);
+    }
 
     public boolean execute(Match match) {
         Cell cell = match.getBoard().getCell(point);
