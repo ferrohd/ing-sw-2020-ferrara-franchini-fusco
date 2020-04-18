abe8dbedcef9c52401e6ad5a6b88c8c2c5e5223c
luigifusco <luigifusco98@gmail.com>
2020-04-18 18:21:37 +0200

    add MinotaurMoveAction

--- src/main/java/it/polimi/ingsw/PSP14/server/actions/MinotaurMoveAction.java
+++ src/main/java/it/polimi/ingsw/PSP14/server/actions/MinotaurMoveAction.java
@@ -4,23 +4,25 @@ import it.polimi.ingsw.PSP14.server.model.Match;
 import it.polimi.ingsw.PSP14.server.model.Player;
 import it.polimi.ingsw.PSP14.server.model.Point;
 
-public class ApolloMoveAction extends MoveAction {
-    public ApolloMoveAction(String user, Point from, Point to) {
+public class MinotaurMoveAction extends MoveAction {
+    public MinotaurMoveAction(String user, Point from, Point to) {
         super(user, from, to);
     }
 
     @Override
     public boolean execute(Match match) {
-        // finds the worker at the "to" position
         for(Player p: match.getPlayers()) {
             for(int i = 0; i < 2; ++i) {
-                if(p.getWorker(i).getPos().equals(getTo())) {
-                    // moves the worker at "from" to  "to"
+                if(p.getWorker(i).getPos().equals(getTo()) &&
+                        !p.getUsername().equals(getUser())) {
                     if(super.execute(match)) {
-                        // moves the worker at "to" to "from"
-                        p.getWorker(i).setPos(getFrom());
+                        Point newPos = new Point(
+                                2*getTo().getX() - getFrom().getX(),
+                                2*getTo().getY() - getFrom().getY()
+                        );
+                        p.getWorker(i).setPos(newPos);
                         return true;
-                    } else { // if normal execution failed
+                    } else {
                         return false;
                     }
                 }
