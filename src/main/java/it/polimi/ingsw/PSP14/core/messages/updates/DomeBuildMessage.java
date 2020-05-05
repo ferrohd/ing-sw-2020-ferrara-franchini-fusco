dfcedd69c9a209537dbff4b0e4bc57137be0be01
luigifusco <luigifusco98@gmail.com>
2020-05-05 15:41:33 +0200

    add UIUpdateMessages

---
+++ src/main/java/it/polimi/ingsw/PSP14/core/messages/updates/DomeBuildMessage.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.PSP14.core.messages.updates;
+
+import it.polimi.ingsw.PSP14.client.ServerConnection;
+import it.polimi.ingsw.PSP14.client.model.UIPoint;
+import it.polimi.ingsw.PSP14.client.view.UI;
+import it.polimi.ingsw.PSP14.server.model.Point;
+
+public class DomeBuildMessage implements UIUpdateMessage {
+    private Point pos;
+
+    public DomeBuildMessage(Point pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public boolean execute(UI ui, ServerConnection serverConnection) {
+        ui.setDome(UIPoint.fromPoint(pos));
+
+        return true;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/PSP14/core/messages/updates/PlayerRegisterMessage.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.PSP14.core.messages.updates;
+
+import it.polimi.ingsw.PSP14.client.ServerConnection;
+import it.polimi.ingsw.PSP14.client.view.UI;
+
+public class PlayerRegisterMessage implements UIUpdateMessage {
+    private String username;
+
+    public PlayerRegisterMessage(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean execute(UI ui, ServerConnection serverConnection) {
+        ui.registerPlayer(username);
+
+        return true;
+    }
+}
