e1c08b4ca162903cea5d2096cca2e0532a88f451
luigifusco <luigifusco98@gmail.com>
2020-05-10 19:29:44 +0200

    remove references to Color in Player model (server)

--- src/main/java/it/polimi/ingsw/PSP14/server/model/Player.java
+++ src/main/java/it/polimi/ingsw/PSP14/server/model/Player.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.PSP14.server.model;
 
-import java.awt.Color;
 import java.util.Random;
 
 import it.polimi.ingsw.PSP14.server.model.gods.God;
@@ -12,7 +11,6 @@ public class Player {
     private String username;
     private God god;
     private Worker[] workers = new Worker[2];
-    private Color color;
 
     /**
      * @param username username of the player to display in game
@@ -21,16 +19,6 @@ public class Player {
         if (username == null || username.equals("")) throw new NullPointerException();
         this.username = username;
         this.god = god;
-
-        setPlayerColor();
-    }
-
-    /**
-     *  Set the player color to a random one
-     */
-    private void setPlayerColor() {
-        Random ran = new Random();
-        color = new Color(ran.nextFloat(), ran.nextFloat(), ran.nextFloat());
     }
 
     /**
@@ -65,11 +53,6 @@ public class Player {
     public String getUsername() {
         return username;
     }
-
-    /**
-     * @return the color
-     */
-    public Color getColor() { return color;}
 
     public God getGod() {
         return god;
